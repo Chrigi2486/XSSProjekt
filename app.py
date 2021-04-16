e987812ec2f360da0eb78fbbaaebb44b21dafcae
Chrigi2486 <christian@cataldo.co.uk>
2021-04-16 15:34:58 +0200

    Added requirements instructions and comments

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from json import dump, load
 from flask import Flask, request, redirect, render_template
 
 
-class App(Flask):
+class App(Flask):  # Backend for the interactive part of our website
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        def check_for_file(path, l=False):
+        def check_for_file(path, l=False):  # Checks for critical files and creates them if they're non existent
             if not os.path.isfile(path):
                 os.makedirs(os.path.dirname(path), exist_ok=True)
                 with open(path, 'w') as wfile:
@@ -26,7 +26,7 @@ app = App(__name__)
 
 @app.route('/', methods=['GET'])
 def get_root():
-    return render_template('main.html')
+    return render_template('main.html')  # returns main.html at the root URL
 
 
 @app.route('/comments/', methods=['GET'])
@@ -37,7 +37,7 @@ def get_comments():
         template = comment_template.read()
     formatted_comments = [template.format(color=random.choice(app.neon_colors), position=('right' if index % 2 else 'left'), **comment) for index, comment in enumerate(comments)]
     joined_comments = '\n'.join(formatted_comments)
-    return render_template('comments.html', comments=joined_comments)
+    return render_template('comments.html', comments=joined_comments)  # returns comments.html formatted with the comments at root/comments/
 
 
 @app.route('/comments/post/', methods=['POST'])
@@ -52,28 +52,22 @@ def post_comment():
     with open('./comments/comments.json', 'w') as comments_file:
         dump(comments, comments_file)
 
-    return redirect('/comments/', code=302)
+    return redirect('/comments/', code=302)  # Creates a new comment and redirects to root/comments/
 
 
 @app.route('/comments/reset/', methods=['POST'])
 def post_reset():
     with open('./comments/comments.json', 'w') as comments_file:
         comments_file.write('[]')
-    return redirect('/comments/', code=302)
+    return redirect('/comments/', code=302)  # removes all the comments and redirects to root/comments/
 
 
 @app.route('/search/', methods=['GET'])
 def get_search():
     search = request.args.get('search')
     if search:
-        return render_template('search.html', search=search)
+        return render_template('search.html', search=search)  # returns search.html formatted with the search word at root/search/
     return render_template('search.html', search='')
-
-
-@app.route('/login', methods=['POST'])
-def post_login():
-    data = dict(request.form)
-    pass
 
 
 if __name__ == '__main__':
